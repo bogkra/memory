da6a03a8618da0dfe3ca11ebd9da265e78eba4b9
Boguslaw Krawczuk <bkrawczuk@wp.pl>
2018-01-30 22:22:23 +0100

    doubled parts of algorithms now unique

--- list2.cpp
+++ list2.cpp
@@ -1,142 +1,144 @@
 #include <iostream>
 #include <memory>
- 
 
 using namespace std;
 
+enum Direction {dirForward, dirBackward };
+
+Direction opposite(const Direction dir)
+{
+   return ( dir == dirForward ? dirBackward : dirForward );
+}
+
 class Node
 {
- public:
-    Node(const int v) :
-        next(nullptr),
-        prev(nullptr),
-        value(v)
-    {}
+public:
+   Node(const int v) :
+       next(nullptr),
+       prev(nullptr),
+       value(v)
+   {}
 
-    shared_ptr<Node> next;
-    shared_ptr<Node> prev;
-    int value;
+   shared_ptr<Node> next;
+   shared_ptr<Node> prev;
+   int value;
+
+   shared_ptr<Node> * adjanced(const Direction dir); // zwraca wskaznik na next albo prev
 };
 
 class List
 {
- public:
-    List();
-    void add(shared_ptr<Node> node);        // dodaje element na koniec listy
-    void addFirst(shared_ptr<Node> node);   // dodaje element na poczatek listy
-    shared_ptr<Node> get(const int value);  // zwraca element o wskazanej wartości
-    shared_ptr<Node> getBackward(const int value);  // zwraca element o wskazanej wartości
+public:
+   List();
 
- private:
-    shared_ptr<Node> first;
-    shared_ptr<Node> last;
+   void addLast(shared_ptr<Node> node);    // dodaje element na koniec listy
+   void addFirst(shared_ptr<Node> node);   // dodaje element na poczatek listy
+   shared_ptr<Node> getForward(const int value);  // zwraca element o wskazanej wartości
+   shared_ptr<Node> getBackward(const int value);  // zwraca element o wskazanej wartości
+
+private:
+   shared_ptr<Node> first;
+   shared_ptr<Node> last;
+   shared_ptr<Node> * edge(const Direction dir); // zwraca wskaznik na first albo last
+   void add(shared_ptr<Node> node, const Direction dir);        // dodaje element na skraj listy
+   shared_ptr<Node> get(const int value, const Direction dir);  // zwraca element o wskazanej wartości
 };
 
 List::List()
-{ 
-  first = nullptr;
-  last = nullptr;
+{
+ first = nullptr;
+ last = nullptr;
+}
+
+shared_ptr<Node> * List::edge(const Direction dir)
+{
+   return ( dir == dirForward ? &last : &first );
+}
+
+shared_ptr<Node> * Node::adjanced(const Direction dir)
+{
+   return ( dir == dirForward ? &next : &prev );
 }
 
 
-
-void List::add(shared_ptr<Node> node)
+void List::add(shared_ptr<Node> node, const Direction dir)
 {
-    if(!first) {
-        node->next = nullptr;
-        node->prev = nullptr;
-        first = node;
-        last = node;
-    }    
-    else {
-	node->prev = last;
-        last->next = node;         
-        node->next = nullptr;
-        last = node;
-    }
+   if (!(*edge(dir))) {
+       node->next = nullptr;
+       node->prev = nullptr;
+       first = node;
+       last = node;
+   }    
+   else {
+       *(node->adjanced(opposite(dir))) = *edge(dir);
+       *((*edge(dir))->adjanced(dir)) = node;  
+       *(node->adjanced(dir)) = nullptr;
+       *edge(dir) = node;
+   }
+}
+
+void List::addLast(shared_ptr<Node> node)
+{
+ List::add(node, dirForward);
 }
 
 void List::addFirst(shared_ptr<Node> node)
 {
-    if(!last) {
-        node->next = nullptr;
-        node->prev = nullptr;
-        first = node;
-        last = node;
-    }
-    else {
-        
-       node->next = first;
-       first->prev = node;         
-       node->prev = nullptr;
-       first = node;        
-    }
+ List::add(node, dirBackward);
 }
 
-shared_ptr<Node> List::get(const int value)
+
+shared_ptr<Node> List::get(const int value, const Direction dir)
 {
-    if(!first) {
-        cout << "List is empty!" << endl;
-        return nullptr;
-    }
-    else  {
-        shared_ptr<Node> current = first;
-        do {
-            if(current->value == value)  {
-                cout << "Found value " << current->value << endl;
-                return current;
-            }
-            else  {
-                cout << "Going through " << current->value << endl;
-                current = current->next;
-            }
-        } while(current);
-        cout << "Not found: value " << value << endl;
-        return nullptr;
-    }
+   if( !first) {
+       cout << "List is empty!" << endl;
+       return nullptr;
+   }
+   else  {
+       shared_ptr<Node> current = *edge(opposite(dir));
+       do {
+           if(current->value == value)  {
+               cout << "Found value " << current->value << endl;
+               return current;
+           }
+           else  {
+               cout << "Going through " << current->value << endl;
+               current = *(current->adjanced(dir));
+           }
+       } while(current);
+       cout << "Not found: value " << value << endl;
+       return nullptr;
+   }
 }
+
+shared_ptr<Node> List::getForward(const int value)
+{
+  return get(value, dirForward);
+}
+
 
 shared_ptr<Node> List::getBackward(const int value)
 {
-    if(!last) {
-        cout << "List is empty!" << endl;
-        return nullptr;
-    }
-    else {
-        shared_ptr<Node> current = last;
-        do {
-            if(current->value == value)  {
-                cout << "Found value " << current->value << endl;
-                return current;
-            }
-            else  {
-                cout << "Going through " << current->value << endl;
-                current = current->prev;
-            }
-        } while(current);
-        cout << "Not found: value " << value << endl;
-        return nullptr;
-    }
+  return get(value, dirBackward);
+
 }
 
 int main()
 {
-    List lista;
-    shared_ptr<Node> node4 = make_shared <Node> (4);
-    shared_ptr<Node> node7 = make_shared <Node> (7);
+   List lista;
+   shared_ptr<Node> node4 = make_shared <Node> (4);
+   shared_ptr<Node> node7 = make_shared <Node> (7);
 
-/*    lista.add(node4);
-    lista.add(make_shared <Node> (2));
-    lista.add(node7);
-    lista.add(make_shared <Node> (9));*/ 
-    lista.addFirst(node4);
-    lista.addFirst(make_shared <Node> (2));
-    lista.addFirst(node7);
-    lista.addFirst(make_shared <Node> (9));    
-    auto node = lista.get(2);
-    node = lista.getBackward(2);
-    
-    return 0;
+   lista.addLast(node4);
+   lista.addLast(make_shared <Node> (2));
+   lista.addLast(node7);
+   lista.addLast(make_shared <Node> (9)); /*
+   lista.addFirst(node4);
+   lista.addFirst(make_shared <Node> (2));
+   lista.addFirst(node7);
+   lista.addFirst(make_shared <Node> (9));    */
+   auto node = lista.getForward(2);
+   node = lista.getBackward(2);
+   
+   return 0;
 }
-
-
